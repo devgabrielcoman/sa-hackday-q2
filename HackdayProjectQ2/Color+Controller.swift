110876a85206f7e60bee04e3709e7c869d82b711
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-06-30 17:29:54 +0100

    updated colors

--- HackdayProjectQ2/Color+Controller.swift
+++ HackdayProjectQ2/Color+Controller.swift
@@ -42,15 +42,30 @@ extension ColorController {
     
     func getColors() {
         
+        
+        
         // get data
         let colors: [ColorModel] = [
-            ColorModel(color: UIColor.black),
-            ColorModel(color: UIColor.white),
-            ColorModel(color: UIColor.red),
-            ColorModel(color: UIColor.green),
-            ColorModel(color: UIColor.blue),
-            ColorModel(color: UIColor.magenta),
-            ColorModel(color: UIColor.yellow)
+            ColorModel(color: UIColor(colorLiteralRed: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 195/255.0, green: 6/255.0, blue: 6/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 255/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 255/255.0, green: 120.0/255.0, blue: 0.0/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 255/255.0, green: 186/255.0, blue: 39/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 253/255.0, green: 194/255.0, blue: 68/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 255/255.0, green: 255/255.0, blue: 0.0/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 227/255.0, green: 229/255.0, blue: 68/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 136/255.0, green: 229/255.0, blue: 69/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 99/255.0, green: 215/255.0, blue: 13/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 83/255.0, green: 171/255.0, blue: 19/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 71/255.0, green: 244/255.0, blue: 91/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 33/255.0, green: 150/255.0, blue: 243/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 13/255.0, green: 101/255.0, blue: 169/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 0.0/255.0, green: 54/255.0, blue: 255/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 255/255.0, green: 0.0/255.0, blue: 139/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 249/255.0, green: 187/255.0, blue: 221/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)),
+            ColorModel(color: UIColor(colorLiteralRed: 150/255.0, green: 98/255.0, blue: 64/255.0, alpha: 1))
         ]
         
         // update
