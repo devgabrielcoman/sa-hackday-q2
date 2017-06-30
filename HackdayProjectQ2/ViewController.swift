99b46500e299d575d01b6ef5c199db530a4affe7
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-06-30 09:34:47 +0100

    start

---
+++ HackdayProjectQ2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackdayProjectQ2
+//
+//  Created by Gabriel Coman on 30/06/2017.
+//  Copyright © 2017 Gabriel Coman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
